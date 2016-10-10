c8533db9fb1aab7706809cd6d1b9ea893956d249
dotzero <mail@dotzero.ru>
2016-10-10 12:36:24 +0300

    Add MathtexPath to example

--- examples/cli/cli.go
+++ examples/cli/cli.go
@@ -11,7 +11,10 @@ func main() {
 	flag.Parse()
 	expr := flag.Arg(0)
 
-	mathtex.MathtexMsgLevel = "0"
+	mathtex.MathtexPath = "/var/www/mathtex.cgi" // Default
+	mathtex.MathtexCachePath = "/var/www/cache/" // Default
+	mathtex.MathtexMsgLevel = "0"                // Set zero-verbosity
+
 	filename, err := mathtex.RenderImage(expr)
 	if err != nil {
 		log.Fatalln("Mathtex error: " + err.Error())
